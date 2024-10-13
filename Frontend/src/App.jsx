f07f2bf921cbf65a716f8030901ff6a33ac588d1
devarsheecodess <devarsheegaunekar29@gmail.com>
2024-10-13 17:02:28 +0530

    Added internal pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -11,6 +11,10 @@ import Login from './components/Login';
 import Signup from './components/Signup';
 import Features from './components/Features';
 import Team from './components/Team';
+import Home from './components/pages/Home';
+import Post from './components/pages/Post';
+import TraceAccounts from './components/pages/TraceAccounts';
+import Video from './components/pages/Video';
 
 const router = createBrowserRouter([
   {
@@ -39,6 +43,15 @@ const router = createBrowserRouter([
     ),
   },
   {
+    path: "/team",
+    element: (
+      <div>
+        <Header />
+        <Team />
+      </div>
+    ),
+  },
+  {
     path: "/features",
     element: (
       <div>
@@ -48,11 +61,42 @@ const router = createBrowserRouter([
     ),
   },
   {
-    path: "/team",
+    path: "/home",
     element: (
       <div>
-        <Header />
-        <Team />
+        <Home />
+      </div>
+    ),
+  },
+  {
+    path: "/post",
+    element: (
+      <div>
+        <Post />
+      </div>
+    ),
+  },
+  {
+    path: "/trace-accounts",
+    element: (
+      <div>
+        <TraceAccounts />
+      </div>
+    ),
+  },
+  {
+    path: "/video",
+    element: (
+      <div>
+        <Video />
+      </div>
+    ),
+  },
+  {
+    path: "/history",
+    element: (
+      <div>
+        <History />
       </div>
     ),
   },
---
+++ Frontend/src/components/pages/TraceAccounts.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TraceAccounts = () => {
+  return (
+    <div>
+      <h1>Trace Accounts</h1>
+    </div>
+  )
+}
+
+export default TraceAccounts
